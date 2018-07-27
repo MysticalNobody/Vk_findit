fe76e5e1b96cab5fba056d91c1dc05cefb155fa3
MysticalNobody <nikhidan@ya.ru>
2018-07-27 06:34:32 +0300

    ОНО БЛЯТЬ ДЕЙСТВИТЕЛЬНО РАБОТАЕТ!!1!

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:findit/classes/app.dart';
 import 'package:findit/classes/config.dart';
 import 'package:findit/screens/game/target.dart';
+import 'package:findit/screens/game/hacked.dart';
 import 'package:findit/services/connection.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
@@ -17,7 +18,7 @@ VideoPlayerController controllerBg;
 VideoPlayerController controllerBgHacked;
 AnimationController animationController;
 int distance;
-var status;
+int status = 1;
 
 bool started = false;
 
@@ -67,10 +68,10 @@ class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
     animationController.repeat();
     Connection.listen("target.dist", (params) {
       setState(() {
-        distance = params[1];
-        status = 2;
-        if (Config.targetId != params[0]) {
-          Config.targetId = params[0];
+        distance = params['distance'];
+        status = params['status'];
+        if (Config.targetId != params['target_id']) {
+          Config.targetId = params['target_id'];
           Config.saveToDB();
         }
       });
@@ -88,8 +89,6 @@ class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
         status = 1;
       });
       Connection.send('user.check_status');
-      // if (!started)
-      //   startSend();
       return false;
     });
 
@@ -108,16 +107,7 @@ class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
         key: _scaffoldKey,
-        body: TargetScreen());
+        body: status > 3 ? TargetScreen(): HackedScreen());
   }
 
-  void startSend() async {
-    var location = new Location();
-    while (true) {
-      var currentLocation = await location.getLocation;
-          Connection
-              .send("user.update_geo", {"longitude": currentLocation["longitude"], "latitude": currentLocation["latitude"]});
-      await Future.delayed(new Duration(seconds: 10));
-    }
-  }
 }
